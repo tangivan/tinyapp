a0f6b18c72ec3ecf167aa25b49e398c8eeec11da
Ivan <ivanxintang@gmail.com>
2021-11-08 13:48:24 -0800

    Add handler for '/urls/:shortURL'

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(req.params);
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
