f6d6bb562a2e4c1f76d2413775ce1856a4b03946
Ivan <ivanxintang@gmail.com>
2021-11-09 19:20:46 -0800

    Install morgan, handler for delete requests, and handler for update requests

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 2002; // default port 8080
 
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const generateRandomString = () => {
   return Math.floor((1 + Math.random()) * 100000000).toString(36);
@@ -21,6 +23,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  console.log(urlDatabase);
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -32,22 +35,35 @@ app.post("/urls", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const { shortURL } = req.params;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(req.params);
+  const { shortURL } = req.params;
+  const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls/:shortURL", (req, res) => {
+  const { shortURL } = req.params;
+  const { newURL } = req.body;
+  console.log('shortURL', shortURL);
+  console.log('newURL', newURL);
+  urlDatabase[shortURL] = newURL;
+  console.log(urlDatabase);
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
